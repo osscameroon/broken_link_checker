19cb970f22b03539900326f82fd5247d60502691
Fomegne <fomegnemeudje@outlook.com>
2022-03-18 14:02:34 +0100

    fix ERROR: Could not find a version that satisfies the requirement urllib3>=1.26.8<2.

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
 from setuptools import setup, find_packages
 
-
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Check the broken links on a website'
 
-with open('README.md', 'r') as f:
+with open('README-PYPI.md', 'r') as f:
     LONG_DESCRIPTION = f.read()
 
 # Setting up
 setup(
-        name="broken_link_checker",
+        name="blc",
         version=VERSION,
         author="pythonbrad",
         author_email="fomegnemeudje@outlook.com",
+        maintainer="Sanix-Darker",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
         packages=find_packages(where="src"),
-        install_requires=["urllib3>=1.26.8<2.0"],
+        install_requires=["urllib3"],
         keywords=["link", "url", "broken", "check"],
         classifiers=[
             "Topic :: Internet :: WWW/HTTP",
         ],
         python_requires='>=3.9',
         package_dir={"": "src"},
+        data_files=[('doc', ['README-PYPI.md'])],
+        url="https://github.com/osscameroon/broken_link_checker",
+        project_urls={
+            'Source': 'https://github.com/osscameroon/broken_link_checker',
+            'Tracker': 'https://github.com/osscameroon/broken_link_checker/issues',
+        },
 )
