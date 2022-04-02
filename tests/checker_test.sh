12e331798208743c735340e8d447bf7a2485e2bf
Fomegne <fomegnemeudje@outlook.com>
2022-04-02 08:24:54 +0100

    test: update Add a server of test to simulate some error.

---
+++ tests/checker_test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Perform some verification on the broken link checker.
+# NB: The purpose of this script is
+# to verify the working of the checker on a real web server.
+
+NB_BROKEN_LINK_EXPECTED=22
+HOST=localhost
+PORT=8080
+counter=5
+
+start_server() {
+    # We start the web server
+    python tests/server.py $HOST $PORT &
+    # We get his pid
+    server_pid=$!
+
+    # We wait the server to start
+    while [ $counter -gt 0 ]; do
+        sleep .1
+        if curl -I $HOST:$PORT -s --show-error; then
+            break
+        else
+            echo Retry\($counter\)
+            counter=$(expr $counter - 1)
+        fi
+    done
+
+    # We verify if the server is run
+    if [ $counter -eq 0 ]; then
+        return 1
+    fi
+}
+
+# We start the test
+start_test() {
+    report=$(python -m broken_link_checker http://$HOST:$PORT -d 0 -D true)
+    nb_broken_link_got=$(expr $(echo "$report" | grep -c .) - 2)
+    if [ ! $nb_broken_link_got -eq $NB_BROKEN_LINK_EXPECTED ]; then
+        echo "$NB_BROKEN_LINK_EXPECTED broken links expected, but $nb_broken_link_got got"
+        echo "REPORT:"
+        echo "$report"
+        return 2
+    fi
+}
+
+# We stop the server
+stop_server() {
+    kill $server_pid
+}
+
+if start_server; then
+    start_test
+fi
+
+err_code=$?
+
+stop_server
+
+exit $err_code
---
+++ tests/server.py
@@ -0,0 +1,107 @@
+"""This server is for test purpose."""
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
+import time
+import os
+import sys
+
+path = os.path.dirname(os.path.realpath(__file__))
+
+if len(sys.argv) < 2:
+    print("Host and Port required")
+else:
+    hostName = sys.argv[1]
+    serverPort = int(sys.argv[2])
+    print(sys.argv)
+
+
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+    """Threading server."""
+
+    pass
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """Define the behavior of the server."""
+
+    def do_GET(self):
+        """All test request will be on this method."""
+        if '?' in self.path:
+            self.path = self.path.split('?')[0]
+
+        if self.path in ['/', '/home', '/abc/']:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+            with open(path+'/index.html', 'rb') as f:
+                self.wfile.write(f.read())
+        elif self.path in ['/abc']:
+            self.send_response(404)
+            self.end_headers()
+        elif self.path == '/error':
+            self.send_response(500)
+            self.end_headers()
+        elif self.path == '/wait':
+            time.sleep(3600)
+        elif self.path == '/empty':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+        elif self.path == '/null':
+            pass
+        elif self.path == '/rss':
+            self.send_response(200)
+            self.send_header("Content-type", "application/rss+xml")
+            self.end_headers()
+
+            with open(path+'/data.rss', 'rb') as f:
+                self.wfile.write(f.read())
+        elif self.path == '/html':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+            with open(path+'/data.html', 'rb') as f:
+                self.wfile.write(f.read())
+        elif self.path == '/html/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+            with open(path+'/data2.html', 'rb') as f:
+                self.wfile.write(f.read())
+        elif self.path.replace('/', '').isdigit():
+            self.send_response(int(self.path.replace('/', '')))
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+        elif self.path == '/good':
+            self.send_response(301)
+            self.send_header('Location', '/201')
+            self.end_headers()
+        elif self.path == '/bad':
+            self.send_response(301)
+            self.send_header('Location', '/401')
+            self.end_headers()
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+    def do_HEAD(sefl):
+        """Permit to check the availability of the test server."""
+        self.send_response(200)
+        self.end_headers()
+
+
+if __name__ == "__main__":
+    server = ThreadingSimpleServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.server_close()
+    print("Server stopped.")
